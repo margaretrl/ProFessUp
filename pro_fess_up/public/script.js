652ca21ce7908c5a729c9f80451d1f3e8fc7e91f
Marija Travoric <marija8t@gmail.com>
2023-10-11 12:54:49 -0400

    fix index html and button navigation

---
+++ pro_fess_up/public/script.js
@@ -0,0 +1,9 @@
+var signInButton = document.querySelector('.signin-link');
+signInButton.addEventListener('click', function () {
+    window.location.href = 'signIn.html';
+});
+
+// var signUpButton = document.querySelector('.signup-link');
+// signInButton.addEventListener('click', function () {
+//     window.location.href = 'signup.html';
+// });

e9b93e2b721d9584afcd88923908d0270dbea96c
Laura Obermaier <lauraaobermaier@gmail.com>
2023-11-06 11:33:14 -0500

    professor add (start)

--- pro_fess_up/public/script.js
+++ pro_fess_up/public/script.js
@@ -3,6 +3,11 @@ signInButton.addEventListener('click', function () {
     window.location.href = 'signIn.html';
 });
 
+var addProfessorButton = document.querySelector('.add-professor-link');
+addProfessorButton.addEventListener('click', function () {
+    window.location.href = 'addProfessor.html';
+});
+
 // var signUpButton = document.querySelector('.signup-link');
 // signInButton.addEventListener('click', function () {
 //     window.location.href = 'signup.html';

6bd1859daf40989d461d8e47a9c9c7fd5f865d85
Laura Obermaier <lauraaobermaier@gmail.com>
2023-12-12 17:33:38 -0500

    delete server changes

--- server/index.js
+++ server/index.js
@@ -397,6 +397,23 @@ app.get('/courses', async (req, res) => {
   }
 });
 
+// GET one course
+app.get('/courses/:id', async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: 'Invalid ID format' });
+  }
+  try {
+      const course = await Courses.findById(id);
+      if (!course) {
+        return res.status(404).json({ message: 'Course not found' });
+      } 
+      res.json(course);
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+});
+
 // Middlewares
 //app.use(express.json());
 
